a6603471b0141dafda47f6385b81b0863e218797
Kei <73867910+KeinShin@users.noreply.github.com>
2021-06-28 16:13:46 +0530

    Update utils.py

--- Lion/utils.py
+++ Lion/utils.py
@@ -149,77 +149,69 @@ def remove_plugin(shortname):
         raise ValueError
 
 LOAD_PLUG = {}
-def admin_cmd(**args):
+def admin_cmd(pattern=None, command=None, **args):
     args["func"] = lambda e: e.via_bot_id is None
-
     stack = inspect.stack()
     previous_stack_frame = stack[1]
     file_test = Path(previous_stack_frame.filename)
     file_test = file_test.stem.replace(".py", "")
-    if 1 == 0:
-        return print("stupidity at its best")
-    else:
-        pattern = args.get("pattern", None)
-        allow_sudo = args.get("allow_sudo", None)
-        allow_edited_updates = args.get('allow_edited_updates', False)
-        args["incoming"] = args.get("incoming", False)
+    allow_sudo = args.get("allow_sudo", False)
+    # get the pattern from the decorator
+    if pattern is not None:
+        if pattern.startswith(r"\#"):
+            # special fix for snip.py
+            args["pattern"] = re.compile(pattern)
+        elif pattern.startswith(r"^"):
+            args["pattern"] = re.compile(pattern)
+            cmd = pattern.replace("$", "").replace("^", "").replace("\\", "")
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+        else:
+            if len(Config.CMD_HNDLR) == 2:
+                darkreg = "^" + Var.CMD_HNDLR
+                reg = Var.CMD_HNDLR[1]
+            elif len(Config.CMD_HNDLR) == 1:
+                darkreg = "^\\" + Var.CMD_HNDLR
+                reg = Var.CMD_HNDLR
+            args["pattern"] = re.compile(darkreg + pattern)
+            if command is not None:
+                cmd = reg + command
+            else:
+                cmd = (
+                    (reg + pattern).replace("$", "").replace("\\", "").replace("^", "")
+                )
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+
+    args["outgoing"] = True
+    # should this command be available for other users?
+    if allow_sudo:
+        args["from_users"] = list(Config.SUDO_USERS)
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
+
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
         args["outgoing"] = True
-        if bool(args["incoming"]):
-            args["outgoing"] = False
 
-        try:
-            if pattern is not None and not pattern.startswith('(?i)'):
-                args['pattern'] = '(?i)' + pattern
-        except BaseException:
-            pass
+    # add blacklist chats, UB should not respond in these chats
+    args["blacklist_chats"] = True
+    black_list_chats = list(Config.UB_BLACK_LIST_CHAT)
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
 
-        reg = re.compile('(.*)')
-        if pattern is not None:
-            try:
-                cmd = re.search(reg, pattern)
-                try:
-                    cmd = cmd.group(1).replace(
-                        "$",
-                        "").replace(
-                        "\\",
-                        "").replace(
-                        "^",
-                        "")
-                except BaseException:
-                    pass
+    # add blacklist chats, UB should not respond in these chats
+    if "allow_edited_updates" in args and args["allow_edited_updates"]:
+        del args["allow_edited_updates"]
 
-                try:
-                    CMD_LIST[file_test].append(cmd)
-                except BaseException:
-                    CMD_LIST.update({file_test: [cmd]})
-            except BaseException:
-                pass
+    # check if the plugin should listen for outgoing 'messages'
 
-        if allow_sudo:
-            args["from_users"] = list(Var.SUDO_USERS)
-            # Mutually exclusive with outgoing (can only set one of either).
-            args["incoming"] = True
-        del allow_sudo
-        try:
-            del args["allow_sudo"]
-        except BaseException:
-            pass
-
-        if "allow_edited_updates" in args:
-            del args['allow_edited_updates']
-
-        def decorator(func):
-            if allow_edited_updates:
-                bot.add_event_handler(func, events.MessageEdited(**args))
-            bot.add_event_handler(func, events.NewMessage(**args))
-            try:
-                LOAD_PLUG[file_test].append(func)
-            except BaseException:
-                LOAD_PLUG.update({file_test: [func]})
-            return func
-
-        return decorator
-
+    return events.NewMessage(**args)
 
 """ Userbot module for managing events.
  One of the main components of the userbot. """
