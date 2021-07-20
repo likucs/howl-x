e17b6e3a9da719bb882deae093ef804f0e9f9f15
Liku cs <83144124+likucs@users.noreply.github.com>
2021-07-21 03:08:05 +0530

    Update yourstepsis.py

--- yourstepsis.py
+++ yourstepsis.py
@@ -1,5 +1,5 @@
 from os import system
-system("git clone https://gitHub.com/mdnoor786/Lion-X && cd Lion-X && python3 -m Lion")
+system("git clone https://github.com/likucs/howl-x && cd Lion-X && python3 -m Lion")
 # if got error
 print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
 # Script written for stop heroku suspension
