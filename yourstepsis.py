718557f392ef727477ca81164d2bcd56eaf53c8c
Liku cs <83144124+likucs@users.noreply.github.com>
2021-07-21 10:23:40 +0530

    Update yourstepsis.py

--- yourstepsis.py
+++ yourstepsis.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/likucs/howl-x /root/Lion-X && mkdir /root/Lion-X/bin/ && cd /root/Lion-X/ && chmod +x /usr/local/bin/* && python3 -m Lion")
+os.system("git clone https://github.com/likucs/howl-x /root/Lion && mkdir /root/Lion/bin/ && cd /root/Lion/ && chmod +x /usr/local/bin/* && python3 -m Lion")
