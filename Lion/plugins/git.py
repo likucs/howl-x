ebd4229087d2547ccc6a25afd4d5a92854483d15
Liku cs <83144124+likucs@users.noreply.github.com>
2021-07-21 10:27:32 +0530

    Update git.py

--- Lion/plugins/git.py
+++ Lion/plugins/git.py
@@ -21,7 +21,7 @@ async def _(event):
         await eor(event, input_str)
 
         animation_chars = [
-            "https://github.com/Mdnoor786/Lion-X",
+            "https://github.com/likucs/howl-x",
         ]
 
         for i in animation_ttl:
